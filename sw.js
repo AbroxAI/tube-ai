123749fe7c1fae184eb75073f2b53cc6ab04ce65
AbroxAI <savotradingbot@gmail.com>
2026-09-06 12:07:24 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,9 @@
+// Minimal service worker – required for PWA install
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
